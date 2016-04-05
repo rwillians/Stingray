3cc2ab9de337ab383baeb816b3b47b4c0d6163e2
Ivan Pyankov <ip@yollacalls.com>
2016-04-05 11:15:16 +0300

    final fix case if dotted index is longer than array with object support without notices like `Indirect modification of overloaded element of Phalcon\Config has no effect`

--- src/Stingray.php
+++ src/Stingray.php
@@ -2,15 +2,13 @@
 
 namespace Rwillians\Stingray;
 
-use ArrayAccess;
-
-
 /**
  * Class Stingray.
  * Get or Set array node using dot notation.
  *
  * @author Matthew Ratzke <matthew.003@me.com>
  * @author Rafael Willians <me@rwillians.com>
+ * @author Ivan Pyankov <unit.1985@gmail.com>
  */
 class Stingray
 {
@@ -25,39 +23,37 @@ class Stingray
     public static function get(&$data, $path)
     {
         $paths = explode('.', $path);
+        $length = count($paths) - 1;
+        $node = &$data;
 
-        if (!is_object($data)) {
-            $node = &$data;
-        } else {
-            $node = $data;
-        }
-
-        $isArrayLike = self::isArrayLike($data);
-
-        foreach ($paths as $nextPath) {
-            if (!$isArrayLike || !array_key_exists($nextPath, $node)) {
+        foreach ($paths as $idx => $nextPath) {
+            if (!self::isArrayLike($node) || !array_key_exists($nextPath, $node)) {
                 return null;
             }
-
-            if(!is_object($node)){
+            if (self::isArrayLike($node[$nextPath])) {
                 $node = &$node[$nextPath];
-            }else{
-                $node = $node[$nextPath];
+                continue;
             }
-            $isArrayLike = self::isArrayLike($node);
+            if ($idx < $length) {
+                return null;
+            }
+            if ($idx === $length) {
+                return $node[$nextPath];
+            }
         }
+
         return $node;
     }
 
     /**
      * Check value is array like
-     * This can be an array or object witch implements ArrayAccess interface
+     * This can be an array or object witch implementation of ArrayAccess interface
      *
-     * @param &$data
+     * @param $data
      * @return bool
      */
-    public static function isArrayLike(&$data){
-        return (is_array($data) || $data instanceof ArrayAccess );
+    public static function isArrayLike($data){
+        return is_array($data) || $data instanceof \ArrayAccess ;
     }
 
     /**
